8b107ca4089bfbdff75b3b0c98824de00771b8cc
Li-Haowei <haoweilee666@gmail.com>
2022-07-11 16:31:35 -0400

    working on video filter

---
+++ VideoCapture.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 16:14:49 2022
+
+@author: Haowei Li
+"""
+import cv2
+
+cap = cv2.VideoCapture("./video.mp4")
+while not cap.isOpened():
+    cap = cv2.VideoCapture("./video.mp4")
+    cv2.waitKey(1000)
+    print("Wait for the header")
+
+CV_CAP_PROP_POS_FRAMES = 1
+pos_frame = cap.get(CV_CAP_PROP_POS_FRAMES)
+while True:
+    flag, frame = cap.read()
+    if flag:
+        # The frame is ready and already captured
+        cv2.imshow('video', frame)
+        pos_frame = cap.get(CV_CAP_PROP_POS_FRAMES)
+        print( str(pos_frame)+" frames")
+    else:
+        # The next frame is not ready, so we try to read it again
+        cap.set(CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+        print( "frame is not ready")
+        # It is better to wait for a while for the next frame to be ready
+        cv2.waitKey(1000)
+
+    if cv2.waitKey(10) == 27:
+        break
+    if cap.get(CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+        # If the number of captured frames is equal to the total number of frames,
+        # we stop
+        break
