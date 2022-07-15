a735c2138277a35e32696614d961237897a26c2b
Li-Haowei <haoweilee666@gmail.com>
2022-07-14 23:36:41 -0400

    Update InterestingVideoFilter.py

--- InterestingVideoFilter.py
+++ InterestingVideoFilter.py
@@ -44,18 +44,23 @@ class InterestingVideoFilter():
         # Closes all the windows currently opened.
         cv2.destroyAllWindows()
     
-    def output(self):
+    def blackAndWhite(self):
         # Loop until the end of the video
         while (self.cap.isOpened()):
          
             # Capture frame-by-frame
             ret, frame = self.cap.read()
-            frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0,
-                                 interpolation = cv2.INTER_CUBIC)
-         
+            try:
+                frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+            except:
+                # release the video capture object
+                self.cap.release()
+                # Closes all the windows currently opened.
+                cv2.destroyAllWindows()
+                break
             # Display the resulting frame
             cv2.imshow('Frame', frame)
-         
+            
             # conversion of BGR to grayscale is necessary to apply this operation
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
          
@@ -68,9 +73,33 @@ class InterestingVideoFilter():
             # define q as the exit button
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
+            
+            
+    def smoothing(self):
+        # Loop until the end of the video
+        while (self.cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            try:
+                frame = cv2.resize(frame, (540, 380), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
+            except:
+                # release the video capture object
+                self.cap.release()
+                # Closes all the windows currently opened.
+                cv2.destroyAllWindows()
+                break
+            # Display the resulting frame
+            cv2.imshow('Frame', frame)
+            print(frame)
+            
+            # using cv2.Gaussianblur() method to blur the video
          
-        # release the video capture object
-        self.cap.release()
-        # Closes all the windows currently opened.
-        cv2.destroyAllWindows()
-#'video.mp4'
+            # (5, 5) is the kernel size for blurring.
+            gaussianblur = cv2.GaussianBlur(frame, (5, 5), 0)
+            cv2.imshow('gblur', gaussianblur)
+         
+            # define q as the exit button
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+iv = InterestingVideoFilter('video.mp4')
+iv.blackAndWhite()
